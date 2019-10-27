b39ab37fff65388c5807cf540d3cbd0ebd89f727
atraya <atraya@csumb.edu>
2019-10-27 02:21:49 -0400

    update

--- app.js
+++ app.js
@@ -12,15 +12,25 @@ app.get("/", function(req, res){
   
 });
 app.get("/mercury", function(req, res){
-    res.render("mercury.html");
+    res.sent("<h1> Mercury Planet </h1>");
+    res.render("mercury.ejs");
 });
 
 app.get("/venus", function(req, res){
-  res.render("venus.html");
+  res.sent("<h1> Venus Planet </h1>");
+  res.render("venus.ejs");
 });
 app.get("/earth",function(req,res){
-  res.render("earth.html");
+  res.sent("<h1> Earth Planet </h1>");
+  res.render("earth.ejs");
 })
-app.listen(process.env.PORT,process.env.IP,function(){
-  console.log("Running Express Server...")
+app.get("/saturn",function(req,res){
+  res.sent("<h1> Saturn Planet </h1>");
+  res.render("saturn.ejs");
+})
+app.listen("8081","0.0.0.0",function(){
+  console.log("Express Server is running...");
 });
+//app.listen(process.env.PORT,process.env.IP,function(){
+  //console.log("Running Express Server...")
+//});
